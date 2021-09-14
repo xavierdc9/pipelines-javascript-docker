00e2107900588b0558f4295815a285301641f278
xavierdc9 <33961281+xavierdc9@users.noreply.github.com>
2021-09-14 06:51:18 -0700

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,8 +1,8 @@
-'use strict';
+/*'use strict';
 
 const express = require('express');
 
-const PORT = 8080;
+const PORT = 5800;
 const HOST = '0.0.0.0';
 
 const app = express();
@@ -12,3 +12,10 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+*/
+ var connect = require('connect');
+ var serveStatic = require('serve-static');
+
+ connect()
+     .use(serveStatic(__dirname))
+     .listen(5800, () => console.log('Server running on 5800...'));
