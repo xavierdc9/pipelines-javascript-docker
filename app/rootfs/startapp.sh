a64b9f56bd13abeb14a3a71704e307968ad7f039
xavierdc9 <33961281+xavierdc9@users.noreply.github.com>
2021-09-16 07:20:44 -0700

    Update startapp.sh

--- app/rootfs/startapp.sh
+++ app/rootfs/startapp.sh
@@ -7,3 +7,4 @@ export HOME=/config
 mkdir -p /config/profile
 firefox --version
 exec /usr/bin/firefox_wrapper --profile /config/profile --setDefaultBrowser >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
+exec npm start server.js
