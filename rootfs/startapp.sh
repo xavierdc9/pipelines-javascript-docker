6d12f6b6879627e651b4f83034249ebfc1d003e0
xavierdc9 <33961281+xavierdc9@users.noreply.github.com>
2021-09-16 00:46:06 -0700

    Update startapp.sh

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-ip a
+#!/usr/bin/with-contenv sh
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+export HOME=/config
+mkdir -p /config/profile
+firefox --version
+exec /usr/bin/firefox_wrapper --profile /config/profile --setDefaultBrowser >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
 #exec /usr/bin/xterm
 #flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 #sleep 5
