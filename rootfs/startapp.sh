2db139381c730cd769e0dff94e5503eda1ee3fb1
xavierdc9 <33961281+xavierdc9@users.noreply.github.com>
2021-09-16 00:44:39 -0700

    Add files via upload

---
+++ rootfs/startapp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ip a
+#exec /usr/bin/xterm
+#flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#sleep 5
+#flatpak install flathub com.github.micahflee.torbrowser-launcher -y
+#sleep 5
+#flatpak run com.github.micahflee.torbrowser-launcher
+#exec torbrowser-launcher
+
+#tar -xf tor-browser-linux64-10.5.5_en-US.tar.xz .
+#tar -zxvf /tor-browser-linux64-10.5.5_en-US.tar.xz
+#cd /tor-browser_en-US
+#chmod +x start-tor-browser.desktop
+#./start-tor-browser.desktop
+#exec /tor-browser_en-US/Browser/start-tor-browser
